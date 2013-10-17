570596a21e619b3feab196902df263829482ff8d
Adam Timberlake <adam.timberlake@gmail.com>
2013-10-17 01:17:50 +0100

    Added basic connection to WS

--- modules/client/Snapshot.js
+++ modules/client/Snapshot.js
@@ -1,5 +1,29 @@
 (function($window) {
 
+    /**
+     * @module Snapshot
+     * @constructor
+     */
+    var Snapshot = function() {
 
+        this.socketIo = io.connect('http://localhost:8888');
+
+    };
+
+    /**
+     * @property prototype
+     * @type {Object}
+     */
+    Snapshot.prototype = {
+
+        /**
+         * @property socketIo
+         * @type {Object}
+         */
+        socketIo: null
+
+    };
+
+    $window.snapshot = new Snapshot();
 
 })(window);
