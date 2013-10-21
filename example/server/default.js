a1dcd0d4be84f9ad2595c106fab499f7c4526acc
Adam Timberlake <adam.timberlake@gmail.com>
2013-10-21 23:58:42 +0100

    Added 100,000 model example

--- example/server/default.js
+++ example/server/default.js
@@ -1,5 +1,5 @@
 var io          = require('socket.io').listen(8888),
-    request     = require('request'),
+    fs          = require('fs'),
     Snapshot    = require('./../../modules/Snapshot.js');
 
 /**
@@ -15,19 +15,12 @@ io.sockets.on('connection', function (socket) {
     $snapshot.setPageNumber(1);
     $snapshot.setSortBy('word', 'ascending');
 
-    // URL to fetch the example JSON data from.
-    var url = 'http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10000&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
+    fs.readFile('./words.json', 'utf8', function(err, data) {
 
-    request(url, function (error, response, content) {
-
-        if (!error && response.statusCode === 200) {
-
-            // When the request is successful, pass the collection to Snapshot's `setCollection`
-            // after parsing the JSON document.
-            var json = JSON.parse(content);
-            $snapshot.setCollection(json);
-
-        }
+        // When the request is successful, pass the collection to Snapshot's `setCollection`
+        // after parsing the JSON document.
+        var json = JSON.parse(data);
+        $snapshot.setCollection(json);
 
     });
 
